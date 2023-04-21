ea9a49d837b0bbef224432cefc938616e49fe914
simon <simonkhartoum@gmail.com>
2023-04-21 11:01:41 +0300

    completed

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -2,7 +2,7 @@ import React from "react";
 import howManyParks from "./parks/howManyParks";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  howManyParks(); 
 
   return <h1>Colorado State Parks!</h1>;
 }
